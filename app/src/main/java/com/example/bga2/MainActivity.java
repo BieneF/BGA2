3246d189fa31ae7563e71f376bc6df24997aefc1
celinefox <celine.fox@iubh-fernstudium.de>
2022-10-11 12:15:08 +0200

    Version1

--- app/src/main/java/com/example/bga2/MainActivity.java
+++ app/src/main/java/com/example/bga2/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout = findViewById(R.id.tabBar);
+        TabItem tabTermine = findViewById(R.id.tabTermine);
+        TabItem tabSpieler = findViewById(R.id.tabSpieler);
+        TabItem tabSpiele = findViewById(R.id.tabSpiele);
+
+
     }
 }
---
+++ app/src/main/java/com/example/bga2/PagerAdapter.java
@@ -0,0 +1,23 @@
+package com.example.bga2;
+
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class PagerAdapter extends FragmentPagerAdapter {
+
+    public PagerAdapter(FragmentManager fragmentManager){
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
